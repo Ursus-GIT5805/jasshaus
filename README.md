b11bc9f04dd96e03fd187a65bd82af10b5309a32
Ursus Wigger <wigger.ursus@gmail.com>
2022-12-28 09:07:48 +0100

    Remove SSL from python-websocket

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ You have to install Python. Version 3.8.10 works, other versions may not work. A
 
 To install all required python-packages run in the main folder:
 ```
-sudo pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 ```
 
 ---
@@ -26,3 +26,14 @@ Simply run in the main folder:
 python3 server/server.py -dev
 ```
 After that open `content/index.html` in a browser and have some fun.
+
+---
+
+## Infos regarding running it on the server
+
+If you want to run this program on a server, you have to do the following:
+
+* Choose a webserver (e.g. Nginx, Apache)
+* Create SSL-Certificates (and use it for the webserver)
+* Create a reverse proxy to redirect the wss://-protocol to port 7999
+* Run `python3 server/server.py` as either a service or manually
--- server/server.py
+++ server/server.py
@@ -1,6 +1,5 @@
 import websockets
 import asyncio
-import ssl
 import sys
 
 import room
@@ -12,7 +11,7 @@ rooms = room.Room()
 async def connection(websocket, path):
     global rooms
 
-    print("New Socket: {}".format(websocket.remote_address[0]))
+    print("New Socket")
 
     # Get player ID of the table
     id = await rooms.register( websocket )
@@ -38,18 +37,11 @@ if __name__ == "__main__":
     for arg in sys.argv[1:]:
         if arg == "-dev": c.DEV_MODE = True
 
-    # Setup websocketserver
-    server = None
+    # Start websocketserver
+    server = websockets.serve(connection, "0.0.0.0", 7999) # On the server, the reverse proxy handles the SSL-verification
 
     print( "Websocketserver started!" )
 
-    if c.DEV_MODE:
-        server = websockets.serve(connection, "0.0.0.0", 7999) # Unsecure server on localhost
-    else:
-        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-        ssl_context.load_cert_chain('../ssl/fullchain.pem', '../ssl/privkey.pem')
-
-        server = websockets.serve(connection, "0.0.0.0", 7999, ssl=ssl_context)
 
     # Run this program for ever
     loop = asyncio.get_event_loop();
